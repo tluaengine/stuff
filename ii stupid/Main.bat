1a6263a0bc61e428375ef450a812867a985754ce
tluaengine <tluaengine@gmail.com>
2026-08-06 00:13:08 +0100

    Add files via upload

---
+++ ii stupid/Main.bat
@@ -0,0 +1,41 @@
+@echo off
+cd /d "%~dp0"
+if not exist "game_dir.txt" call get_game_dir.bat
+set /p game_dir=<game_dir.txt
+echo game directory?: %game_dir%
+
+if not exist "%game_dir%" (
+	echo change ur ac diractory&pause&exit /b
+)
+if not exist "%game_dir%\AnimalCompany.exe" (
+	start "" "steam://validate/4551040"
+	echo validating ac now. relanuch this when it's done.
+	pause
+	exit
+)
+
+cd menu stuff
+start "" bypass.py
+echo wait for the other window to say you can launch ac
+
+:wait
+tasklist /FI "IMAGENAME eq AnimalCompany.exe" | find /I "AnimalCompany.exe" >nul
+if not errorlevel 1 (
+    goto good
+)
+
+tasklist /FI "IMAGENAME eq EACLauncher.exe" | find /I "EACLauncher.exe" >nul
+if not errorlevel 1 (
+    goto good
+)
+goto wait
+:good
+echo ac detected waiting to load menu
+timeout /t 3 /nobreak >nul
+start "" questservers.bat
+set /p eac=<is_eaclauncher.txt
+if "%eac%"=="1" (
+    call "Stupidity (EACLauncher).bat"
+) else (
+    call "Stupidity (AnimalCompany).bat"
+)
