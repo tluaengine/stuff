ed77d7f6c7cf5b955199eb8f3daffc61dc1297b4
tluaengine <tluaengine@gmail.com>
2026-08-17 20:36:16 +0100

    Delete bb465 directory

--- bb465/Mod/m4literallymadethisinaminute.ts
+++
@@ -1,5 +0,0 @@
-
-Il2Cpp.perform(() => {
-	Il2Cpp.domain.assembly("AnimalCompany").image.class("AnimalCompany.App").method("get_state").invoke().method("get_user").invoke().method("get_isDeveloper").invoke().method("set_value").invoke(true);
-	console.log("okay if you see this then it worked, you have developer mode enabled now.");
-});
